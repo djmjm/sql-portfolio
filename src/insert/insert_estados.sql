e3ec44ca59e63d644b337eddb133b29bdf4f5826
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-07-10 17:35:33 -0300

    Insert com todos estados brasileiros.

---
+++ src/insert/insert_estados.sql
@@ -0,0 +1,35 @@
+insert into estados 
+	(nome, sigla, 
+	regiao, populacao)
+	values
+	( 'Pernanbuco', 'PE', 'Nordeste', 9.278 ),
+	( 'Maranhão', 'MA', 'Nordeste', 6.851 ),
+	( 'Piaui', 'PI', 'Nordeste', 3.195 ),
+	( 'Ceará', 'CE', 'Nordeste', 8.843 ),
+	( 'Rio Grande do Norte', 'RN', 'Nordeste', 3.409 ),
+	( 'Paraíba', 'PB', 'Nordeste', 3.944 ),
+	( 'Alagoas', 'AL', 'Nordeste', 3.322 ),
+	( 'Sergipe', 'SE', 'Nordeste', 2.220 ),
+	( 'Bahia', 'BA', 'Nordeste', 15.130 ),
+
+	( 'Acre', 'AC', 'Norte', 0.970 ),
+	( 'Amapá', 'AP', 'Norte', 0.751 ),
+	( 'Pará', 'PA', 'Norte', 8.074 ),
+	( 'Rondônia', 'RO', 'Norte', 1.749 ),
+	( 'Roraima', 'RR', 'Norte', 0.496 ),
+	( 'Tocantins', 'TO', 'Norte', 1.497 ),
+
+	( 'Goiás', 'GO', 'Centro-Oeste', 6.523 ),
+	( 'Mato Grosso', 'MT', 'Centro-Oeste', 3.224 ),
+	( 'Mato Grosso do Sul', 'MS', 'Centro-Oeste', 2.620 ),
+	( 'Distrito Federal', 'DF', 'Centro-Oeste', 3.094 ),
+
+	( 'Espírito Santo', 'ES', 'Sudeste', 3.885 ),
+	( 'Minas Gerais', 'MG', 'Sudeste', 20.870 ),
+	( 'Rio de Janeiro', 'RJ', 'Sudeste', 16.460 ),
+	( 'São Paulo', 'SP', 'Sudeste', 44.040 ),
+
+	( 'Paraná', 'PR', 'Sul', 11.080 ),
+	( 'Santa Catarina', 'SC', 'Sul', 7.165 ),
+	( 'Rio Grande do Sul', 'RS', 'Sul', 11.290 )
+;

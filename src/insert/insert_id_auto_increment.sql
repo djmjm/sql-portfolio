0f4167ef69c87d581d3887a3abeceb7eb7862fd8
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-07-11 16:56:20 -0300

    Exemplo de insert com id específico e auto_increment.

---
+++ src/insert/insert_id_auto_increment.sql
@@ -0,0 +1,11 @@
+insert into estados
+	(id, nome, sigla, regiao, populacao)
+	values
+	(1000, 'Estado fake', 'NV', 'sul', 2.55)
+;
+
+insert into estados
+	(nome, sigla, regiao, populacao)
+	values
+	('Estado mais novo fake', 'MV', 'norte', 9.55)
+;
